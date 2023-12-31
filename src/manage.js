8aacd8b3a5ae20606dee7368f1b3438a48955567
Szymon Nowicki <sznowicki@eskalacja.com>
2023-12-31 13:42:52 +0100

    style: structure manage.js better

--- src/manage.js
+++ src/manage.js
@@ -1,5 +1,7 @@
 import {meiliClient} from './helpers/meili.js';
 import {MEILI_INDEX_PREFIX} from './helpers/constants.js';
+import {getMongo} from './helpers/mongoRegister.js';
+import {wait} from './helpers/wait.js';
 
 
 const indexSettings = {
@@ -10,13 +12,17 @@ const indexSettings = {
 	sortableAttributes: ['crawledAt'],
 };
 
-const main = async () => {
+const setupIndex = async () => {
 	await meiliClient.createIndex(`${MEILI_INDEX_PREFIX}blogs`);
 	await meiliClient.createIndex(`${MEILI_INDEX_PREFIX}docs`);
 	await meiliClient.createIndex(`${MEILI_INDEX_PREFIX}magazines`);
 	await meiliClient.index(`${MEILI_INDEX_PREFIX}blogs`).updateSettings(indexSettings);
 	await meiliClient.index(`${MEILI_INDEX_PREFIX}docs`).updateSettings(indexSettings);
 	await meiliClient.index(`${MEILI_INDEX_PREFIX}magazines`).updateSettings(indexSettings);
+};
+
+const main = async () => {
+	await setupIndex();
 };
 
 main();
