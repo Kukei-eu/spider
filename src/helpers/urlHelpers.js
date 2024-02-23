94fcc3b35a5f416eba9b08fa5b3318973f5511dc
Szymon Nowicki <sznowicki@eskalacja.com>
2024-02-23 22:06:44 +0100

    fix: better filters for smaller index

--- src/helpers/urlHelpers.js
+++ src/helpers/urlHelpers.js
@@ -59,6 +59,8 @@ const githubFilter = (url) => {
 	if (url.startsWith('https://docs.github.com/') === false) return false;
 
 	if (url.includes('enterprise-server@')) return true;
+
+	return false;
 };
 
 const phpFilter = (url) => {
